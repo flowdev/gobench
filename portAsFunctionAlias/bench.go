3a9442b5482c804d398d96d50653e51185e4cd3a
sb_olebulbuk <ole@sensorberg.com>
2016-01-15 18:53:46 +0100

    Switched to aliasing for SetOutPort too and added some tests for function aliases and interfaces.

--- portAsFunctionAlias/bench.go
+++ portAsFunctionAlias/bench.go
@@ -4,27 +4,27 @@ import (
 	"fmt"
 )
 
-
 // -------- Inc10MFlow
 
 type Inc10MFlow struct {
-	op01      *Inc1MFlow
-	op02      *Inc1MFlow
-	op03      *Inc1MFlow
-	op04      *Inc1MFlow
-	op05      *Inc1MFlow
-	op06      *Inc1MFlow
-	op07      *Inc1MFlow
-	op08      *Inc1MFlow
-	op09      *Inc1MFlow
-	op10      *Inc1MFlow
+	op01       *Inc1MFlow
+	op02       *Inc1MFlow
+	op03       *Inc1MFlow
+	op04       *Inc1MFlow
+	op05       *Inc1MFlow
+	op06       *Inc1MFlow
+	op07       *Inc1MFlow
+	op08       *Inc1MFlow
+	op09       *Inc1MFlow
+	op10       *Inc1MFlow
 	InPort     func(int)
+	SetOutPort func(func(int))
 }
 
 func NewInc10MFlow() *Inc10MFlow {
 	f := &Inc10MFlow{NewInc1MFlow(), NewInc1MFlow(), NewInc1MFlow(),
 		NewInc1MFlow(), NewInc1MFlow(), NewInc1MFlow(), NewInc1MFlow(),
-		NewInc1MFlow(), NewInc1MFlow(), NewInc1MFlow(), nil}
+		NewInc1MFlow(), NewInc1MFlow(), NewInc1MFlow(), nil, nil}
 
 	f.op01.SetOutPort(f.op02.InPort)
 	f.op02.SetOutPort(f.op03.InPort)
@@ -36,35 +36,32 @@ func NewInc10MFlow() *Inc10MFlow {
 	f.op08.SetOutPort(f.op09.InPort)
 	f.op09.SetOutPort(f.op10.InPort)
 	f.InPort = f.op01.InPort
+	f.SetOutPort = f.op10.SetOutPort
 
 	return f
 }
 
-func (f *Inc10MFlow) SetOutPort(outPort func(int)) {
-	f.op10.SetOutPort(outPort)
-}
-
-
 // -------- Inc1MFlow
 
 type Inc1MFlow struct {
-	op01      *Inc100KFlow
-	op02      *Inc100KFlow
-	op03      *Inc100KFlow
-	op04      *Inc100KFlow
-	op05      *Inc100KFlow
-	op06      *Inc100KFlow
-	op07      *Inc100KFlow
-	op08      *Inc100KFlow
-	op09      *Inc100KFlow
-	op10      *Inc100KFlow
+	op01       *Inc100KFlow
+	op02       *Inc100KFlow
+	op03       *Inc100KFlow
+	op04       *Inc100KFlow
+	op05       *Inc100KFlow
+	op06       *Inc100KFlow
+	op07       *Inc100KFlow
+	op08       *Inc100KFlow
+	op09       *Inc100KFlow
+	op10       *Inc100KFlow
 	InPort     func(int)
+	SetOutPort func(func(int))
 }
 
 func NewInc1MFlow() *Inc1MFlow {
 	f := &Inc1MFlow{NewInc100KFlow(), NewInc100KFlow(), NewInc100KFlow(),
 		NewInc100KFlow(), NewInc100KFlow(), NewInc100KFlow(), NewInc100KFlow(),
-		NewInc100KFlow(), NewInc100KFlow(), NewInc100KFlow(), nil}
+		NewInc100KFlow(), NewInc100KFlow(), NewInc100KFlow(), nil, nil}
 
 	f.op01.SetOutPort(f.op02.InPort)
 	f.op02.SetOutPort(f.op03.InPort)
@@ -76,35 +73,32 @@ func NewInc1MFlow() *Inc1MFlow {
 	f.op08.SetOutPort(f.op09.InPort)
 	f.op09.SetOutPort(f.op10.InPort)
 	f.InPort = f.op01.InPort
+	f.SetOutPort = f.op10.SetOutPort
 
 	return f
 }
 
-func (f *Inc1MFlow) SetOutPort(outPort func(int)) {
-	f.op10.SetOutPort(outPort)
-}
-
-
 // -------- Inc100KFlow
 
 type Inc100KFlow struct {
-	op01      *Inc10KFlow
-	op02      *Inc10KFlow
-	op03      *Inc10KFlow
-	op04      *Inc10KFlow
-	op05      *Inc10KFlow
-	op06      *Inc10KFlow
-	op07      *Inc10KFlow
-	op08      *Inc10KFlow
-	op09      *Inc10KFlow
-	op10      *Inc10KFlow
+	op01       *Inc10KFlow
+	op02       *Inc10KFlow
+	op03       *Inc10KFlow
+	op04       *Inc10KFlow
+	op05       *Inc10KFlow
+	op06       *Inc10KFlow
+	op07       *Inc10KFlow
+	op08       *Inc10KFlow
+	op09       *Inc10KFlow
+	op10       *Inc10KFlow
 	InPort     func(int)
+	SetOutPort func(func(int))
 }
 
 func NewInc100KFlow() *Inc100KFlow {
 	f := &Inc100KFlow{NewInc10KFlow(), NewInc10KFlow(), NewInc10KFlow(),
 		NewInc10KFlow(), NewInc10KFlow(), NewInc10KFlow(), NewInc10KFlow(),
-		NewInc10KFlow(), NewInc10KFlow(), NewInc10KFlow(), nil}
+		NewInc10KFlow(), NewInc10KFlow(), NewInc10KFlow(), nil, nil}
 
 	f.op01.SetOutPort(f.op02.InPort)
 	f.op02.SetOutPort(f.op03.InPort)
@@ -116,35 +110,32 @@ func NewInc100KFlow() *Inc100KFlow {
 	f.op08.SetOutPort(f.op09.InPort)
 	f.op09.SetOutPort(f.op10.InPort)
 	f.InPort = f.op01.InPort
+	f.SetOutPort = f.op10.SetOutPort
 
 	return f
 }
 
-func (f *Inc100KFlow) SetOutPort(outPort func(int)) {
-	f.op10.SetOutPort(outPort)
-}
-
-
 // -------- Inc10KFlow
 
 type Inc10KFlow struct {
-	op01      *Inc1KFlow
-	op02      *Inc1KFlow
-	op03      *Inc1KFlow
-	op04      *Inc1KFlow
-	op05      *Inc1KFlow
-	op06      *Inc1KFlow
-	op07      *Inc1KFlow
-	op08      *Inc1KFlow
-	op09      *Inc1KFlow
-	op10      *Inc1KFlow
+	op01       *Inc1KFlow
+	op02       *Inc1KFlow
+	op03       *Inc1KFlow
+	op04       *Inc1KFlow
+	op05       *Inc1KFlow
+	op06       *Inc1KFlow
+	op07       *Inc1KFlow
+	op08       *Inc1KFlow
+	op09       *Inc1KFlow
+	op10       *Inc1KFlow
 	InPort     func(int)
+	SetOutPort func(func(int))
 }
 
 func NewInc10KFlow() *Inc10KFlow {
 	f := &Inc10KFlow{NewInc1KFlow(), NewInc1KFlow(), NewInc1KFlow(),
 		NewInc1KFlow(), NewInc1KFlow(), NewInc1KFlow(), NewInc1KFlow(),
-		NewInc1KFlow(), NewInc1KFlow(), NewInc1KFlow(), nil}
+		NewInc1KFlow(), NewInc1KFlow(), NewInc1KFlow(), nil, nil}
 
 	f.op01.SetOutPort(f.op02.InPort)
 	f.op02.SetOutPort(f.op03.InPort)
@@ -156,35 +147,32 @@ func NewInc10KFlow() *Inc10KFlow {
 	f.op08.SetOutPort(f.op09.InPort)
 	f.op09.SetOutPort(f.op10.InPort)
 	f.InPort = f.op01.InPort
+	f.SetOutPort = f.op10.SetOutPort
 
 	return f
 }
 
-func (f *Inc10KFlow) SetOutPort(outPort func(int)) {
-	f.op10.SetOutPort(outPort)
-}
-
-
 // -------- Inc1KFlow
 
 type Inc1KFlow struct {
-	op01      *Inc100Flow
-	op02      *Inc100Flow
-	op03      *Inc100Flow
-	op04      *Inc100Flow
-	op05      *Inc100Flow
-	op06      *Inc100Flow
-	op07      *Inc100Flow
-	op08      *Inc100Flow
-	op09      *Inc100Flow
-	op10      *Inc100Flow
+	op01       *Inc100Flow
+	op02       *Inc100Flow
+	op03       *Inc100Flow
+	op04       *Inc100Flow
+	op05       *Inc100Flow
+	op06       *Inc100Flow
+	op07       *Inc100Flow
+	op08       *Inc100Flow
+	op09       *Inc100Flow
+	op10       *Inc100Flow
 	InPort     func(int)
+	SetOutPort func(func(int))
 }
 
 func NewInc1KFlow() *Inc1KFlow {
 	f := &Inc1KFlow{NewInc100Flow(), NewInc100Flow(), NewInc100Flow(),
 		NewInc100Flow(), NewInc100Flow(), NewInc100Flow(), NewInc100Flow(),
-		NewInc100Flow(), NewInc100Flow(), NewInc100Flow(), nil}
+		NewInc100Flow(), NewInc100Flow(), NewInc100Flow(), nil, nil}
 
 	f.op01.SetOutPort(f.op02.InPort)
 	f.op02.SetOutPort(f.op03.InPort)
@@ -196,35 +184,32 @@ func NewInc1KFlow() *Inc1KFlow {
 	f.op08.SetOutPort(f.op09.InPort)
 	f.op09.SetOutPort(f.op10.InPort)
 	f.InPort = f.op01.InPort
+	f.SetOutPort = f.op10.SetOutPort
 
 	return f
 }
 
-func (f *Inc1KFlow) SetOutPort(outPort func(int)) {
-	f.op10.SetOutPort(outPort)
-}
-
-
 // -------- Inc100Flow
 
 type Inc100Flow struct {
-	op01      *Inc10Flow
-	op02      *Inc10Flow
-	op03      *Inc10Flow
-	op04      *Inc10Flow
-	op05      *Inc10Flow
-	op06      *Inc10Flow
-	op07      *Inc10Flow
-	op08      *Inc10Flow
-	op09      *Inc10Flow
-	op10      *Inc10Flow
+	op01       *Inc10Flow
+	op02       *Inc10Flow
+	op03       *Inc10Flow
+	op04       *Inc10Flow
+	op05       *Inc10Flow
+	op06       *Inc10Flow
+	op07       *Inc10Flow
+	op08       *Inc10Flow
+	op09       *Inc10Flow
+	op10       *Inc10Flow
 	InPort     func(int)
+	SetOutPort func(func(int))
 }
 
 func NewInc100Flow() *Inc100Flow {
 	f := &Inc100Flow{NewInc10Flow(), NewInc10Flow(), NewInc10Flow(),
 		NewInc10Flow(), NewInc10Flow(), NewInc10Flow(), NewInc10Flow(),
-		NewInc10Flow(), NewInc10Flow(), NewInc10Flow(), nil}
+		NewInc10Flow(), NewInc10Flow(), NewInc10Flow(), nil, nil}
 
 	f.op01.SetOutPort(f.op02.InPort)
 	f.op02.SetOutPort(f.op03.InPort)
@@ -236,34 +221,31 @@ func NewInc100Flow() *Inc100Flow {
 	f.op08.SetOutPort(f.op09.InPort)
 	f.op09.SetOutPort(f.op10.InPort)
 	f.InPort = f.op01.InPort
+	f.SetOutPort = f.op10.SetOutPort
 
 	return f
 }
-
-func (f *Inc100Flow) SetOutPort(outPort func(int)) {
-	f.op10.SetOutPort(outPort)
-}
-
 
 // -------- Inc10Flow
 
 type Inc10Flow struct {
-	op01      *IncOp
-	op02      *IncOp
-	op03      *IncOp
-	op04      *IncOp
-	op05      *IncOp
-	op06      *IncOp
-	op07      *IncOp
-	op08      *IncOp
-	op09      *IncOp
-	op10      *IncOp
+	op01       *IncOp
+	op02       *IncOp
+	op03       *IncOp
+	op04       *IncOp
+	op05       *IncOp
+	op06       *IncOp
+	op07       *IncOp
+	op08       *IncOp
+	op09       *IncOp
+	op10       *IncOp
 	InPort     func(int)
+	SetOutPort func(func(int))
 }
 
 func NewInc10Flow() *Inc10Flow {
 	f := &Inc10Flow{NewIncOp(), NewIncOp(), NewIncOp(), NewIncOp(), NewIncOp(),
-		NewIncOp(), NewIncOp(), NewIncOp(), NewIncOp(), NewIncOp(), nil}
+		NewIncOp(), NewIncOp(), NewIncOp(), NewIncOp(), NewIncOp(), nil, nil}
 
 	f.op01.SetOutPort(f.op02.InPort)
 	f.op02.SetOutPort(f.op03.InPort)
@@ -275,14 +257,10 @@ func NewInc10Flow() *Inc10Flow {
 	f.op08.SetOutPort(f.op09.InPort)
 	f.op09.SetOutPort(f.op10.InPort)
 	f.InPort = f.op01.InPort
+	f.SetOutPort = f.op10.SetOutPort
 
 	return f
 }
-
-func (f *Inc10Flow) SetOutPort(outPort func(int)) {
-	f.op10.SetOutPort(outPort)
-}
-
 
 // -------- single op
 
@@ -302,43 +280,42 @@ func (o *IncOp) SetOutPort(outPort func(int)) {
 	o.outPort = outPort
 }
 
-
 // -------- main
 
 func main() {
 	var result int
 	flow10 := NewInc10Flow()
-	flow10.SetOutPort(func(i int){ result = i })
+	flow10.SetOutPort(func(i int) { result = i })
 	flow10.InPort(0)
 	fmt.Println("10:\t", result)
 
 	flow100 := NewInc100Flow()
-	flow100.SetOutPort(func(i int){ result = i })
+	flow100.SetOutPort(func(i int) { result = i })
 	flow100.InPort(0)
 	fmt.Println("100:\t", result)
 
 	flow1K := NewInc1KFlow()
-	flow1K.SetOutPort(func(i int){ result = i })
+	flow1K.SetOutPort(func(i int) { result = i })
 	flow1K.InPort(0)
 	fmt.Println("1K:\t", result)
 
 	flow10K := NewInc10KFlow()
-	flow10K.SetOutPort(func(i int){ result = i })
+	flow10K.SetOutPort(func(i int) { result = i })
 	flow10K.InPort(0)
 	fmt.Println("10K:\t", result)
 
 	flow100K := NewInc100KFlow()
-	flow100K.SetOutPort(func(i int){ result = i })
+	flow100K.SetOutPort(func(i int) { result = i })
 	flow100K.InPort(0)
 	fmt.Println("100K:\t", result)
 
 	flow1M := NewInc1MFlow()
-	flow1M.SetOutPort(func(i int){ result = i })
+	flow1M.SetOutPort(func(i int) { result = i })
 	flow1M.InPort(0)
 	fmt.Println("1M:\t", result)
 
 	flow10M := NewInc10MFlow()
-	flow10M.SetOutPort(func(i int){ result = i })
+	flow10M.SetOutPort(func(i int) { result = i })
 	flow10M.InPort(0)
 	fmt.Println("10M:\t", result)
 }
--- portAsFunctionAlias/bench_test.go
+++ portAsFunctionAlias/bench_test.go
@@ -1,11 +1,71 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
+
+type Input interface {
+	InPort(i int)
+}
+type Output interface {
+	SetOutPort(func(i int))
+}
+
+func TestInterfaces(t *testing.T) {
+	var result int
+	op := NewIncOp()
+
+	flow := NewInc10Flow()
+	flow.SetOutPort(func(i int) { result = i })
+
+	var value interface{}
+	value = op
+	switch val := value.(type) {
+	case Output:
+		val.SetOutPort(func(i int) { result = i })
+	default:
+		t.Errorf("Unable to convert operation (type %T) to interface 'Output'.\n", value)
+	}
+	switch val := value.(type) {
+	case Input:
+		val.InPort(0)
+	default:
+		t.Errorf("Unable to convert operation (type %T) to interface 'Input'.\n", value)
+	}
+
+	value = flow
+	switch value.(type) {
+	case Output:
+		t.Errorf("Able to convert flow (type %T) to interface 'Output'.\n", value)
+	default:
+		t.Logf("The flow (type %T) doesn't implement the interface 'Output' because the functions belong to the struct itself and not a pointer to it.\n", value)
+	}
+	switch value.(type) {
+	case Input:
+		t.Errorf("Able to convert flow (type %T) to interface 'Input'.\n", value)
+	default:
+		t.Logf("The flow (type %T) doesn't implement the interface 'Input' because the functions belong to the struct itself and not a pointer to it.\n", value)
+	}
+
+	value = *flow
+	switch value.(type) {
+	case Output:
+		t.Errorf("Able to convert flow (type %T) to interface 'Output'.\n", value)
+	default:
+		t.Logf("The flow (type %T) still doesn't implement the interface 'Output' because the functions are fields and not methods. :-(\n", value)
+	}
+	switch value.(type) {
+	case Input:
+		t.Errorf("Able to convert flow (type %T) to interface 'Input'.\n", value)
+	default:
+		t.Logf("The flow (type %T) still doesn't implement the interface 'Input' because the functions are fields and not methods. :-(\n", value)
+	}
+}
 
 func BenchmarkInc10Flow(b *testing.B) {
 	var result int
 	flow := NewInc10Flow()
-	flow.SetOutPort(func(i int){ result = i })
+	flow.SetOutPort(func(i int) { result = i })
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		flow.InPort(0)
@@ -15,7 +75,7 @@ func BenchmarkInc10Flow(b *testing.B) {
 func BenchmarkInc100Flow(b *testing.B) {
 	var result int
 	flow := NewInc100Flow()
-	flow.SetOutPort(func(i int){ result = i })
+	flow.SetOutPort(func(i int) { result = i })
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		flow.InPort(0)
@@ -25,7 +85,7 @@ func BenchmarkInc100Flow(b *testing.B) {
 func BenchmarkInc1KFlow(b *testing.B) {
 	var result int
 	flow := NewInc1KFlow()
-	flow.SetOutPort(func(i int){ result = i })
+	flow.SetOutPort(func(i int) { result = i })
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		flow.InPort(0)
@@ -35,7 +95,7 @@ func BenchmarkInc1KFlow(b *testing.B) {
 func BenchmarkInc10KFlow(b *testing.B) {
 	var result int
 	flow := NewInc10KFlow()
-	flow.SetOutPort(func(i int){ result = i })
+	flow.SetOutPort(func(i int) { result = i })
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		flow.InPort(0)
@@ -45,7 +105,7 @@ func BenchmarkInc10KFlow(b *testing.B) {
 func BenchmarkInc100KFlow(b *testing.B) {
 	var result int
 	flow := NewInc100KFlow()
-	flow.SetOutPort(func(i int){ result = i })
+	flow.SetOutPort(func(i int) { result = i })
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		flow.InPort(0)
@@ -55,7 +115,7 @@ func BenchmarkInc100KFlow(b *testing.B) {
 func BenchmarkInc1MFlow(b *testing.B) {
 	var result int
 	flow := NewInc1MFlow()
-	flow.SetOutPort(func(i int){ result = i })
+	flow.SetOutPort(func(i int) { result = i })
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		flow.InPort(0)
@@ -65,7 +125,7 @@ func BenchmarkInc1MFlow(b *testing.B) {
 func BenchmarkInc10MFlow(b *testing.B) {
 	var result int
 	flow := NewInc10MFlow()
-	flow.SetOutPort(func(i int){ result = i })
+	flow.SetOutPort(func(i int) { result = i })
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		flow.InPort(0)
